3235b1f42047738e7d21dd268cf4bd4751b0a96b
Nikita Solonitsyn <nikita2school@icloud.com>
2025-08-29 18:11:51 +0300

    Prokudin Gorsky done

--- Depth_Vision_and_Graphics/Demosaicing/bayer.py
+++ Depth_Vision_and_Graphics/Demosaicing/bayer.py
@@ -1,4 +1,6 @@
 import numpy as np
+from math import log10
+from numpy.lib.stride_tricks import sliding_window_view
 def get_bayer_masks(n_rows, n_cols):
     """
     :param n_rows: `int`, number of rows
@@ -45,7 +47,23 @@ def get_raw_img(colored_img):
     return np.sum(colored_img*gt_masks, axis=-1)
     
 
-
+def apply_filter(img, filter):
+    """
+    img and filter should have the same amount of channels
+    """
+    if(img.ndim==2):
+        img=img[..., np.newaxis]
+    if(filter.ndim==2):
+        filter=filter[..., np.newaxis]
+    h, w, c=img.shape
+    a, b, c1 =filter.shape
+    assert c1==c
+    assert a%2==1 and b %2==1
+    img=np.pad(img, (((a-1)//2, (a-1)//2), ((b-1)//2, (b-1)//2), (0,0)), mode="constant")
+    windows = sliding_window_view(img, window_shape=(a, b), axis=(0, 1)) #( w0, h0, window1, window2, c)
+    new_img=(windows*np.transpose(filter, (2, 0, 1))).sum(axis=(3,4))
+    return new_img
+            
 def bilinear_interpolation(raw_img):
     """
     :param raw_img:
@@ -57,20 +75,14 @@ def bilinear_interpolation(raw_img):
         result of bilinear interpolation
     """
     h, w=raw_img.shape
-    print(h, w)
     colored_img=get_colored_img(raw_img)
-    sum=np.zeros((h+2, w+2, 3), dtype="uint8")
-    for i in range(3):
-        for j in range(3):
-            sum+=np.pad(colored_img, ((i, 2-i), (j, 2-j), (0,0)), "constant")
-    sum=sum[1:-1, 1:-1]
+    sum=apply_filter(colored_img, np.ones((3, 3, 3)))
     red_mask, green_mask, blue_mask=np.dsplit(get_bayer_masks(h, w), 3)
     green_mask=green_mask.squeeze()
     sum[green_mask]//=2
     sum[~green_mask]//=4
     sum[get_bayer_masks(h, w)]=colored_img[get_bayer_masks(h, w)]
-    print(sum)
-    return sum
+    return sum.astype("uint8")
     
     
 
@@ -83,8 +95,56 @@ def improved_interpolation(raw_img):
         `np.array` of shape `(n_rows, n_cols, 3)` and dtype `np.uint8`,
         result of improved interpolation
     """
-    ...
-
+    G_at_R=np.array(#G at R locations
+        [[0, 0, -1/4, 0, 0],
+        [0, 0, 0, 0, 0],
+        [-1/4, 0, 1, 0, -1/4],
+        [0, 0, 0, 0, 0],
+        [0, 0, -1/4, 0, 0]],
+        dtype=np.float32
+    )
+    G_at_B=G_at_R#G at B locations
+    R_at_G_in_R_B=np.array(#R at green in R row, B column
+        [[0, 0, 1/10, 0, 0],
+        [0, -1/5, 0, -1/5, 0],
+        [-1/5, 0, 1, 0, -1/5],
+        [0, -1/5, 0, -1/5, 0],
+        [0, 0, 1/10, 0, 0]],
+        dtype=np.float32
+    )
+    R_at_G_in_B_R=R_at_G_in_R_B.T#R at green in B row, R column
+    R_at_B=np.array( #R at blue in B row, B column
+        [[0,0,-3/12, 0,0],
+        [0, 0, 0, 0,0],
+        [-3/12, 0, 1, 0, -3/12],
+        [0, 0, 0, 0, 0],
+        [0, 0, -3/12, 0, 0]],
+        dtype=np.float32
+    )
+    B_at_G_in_B_R=G_at_R#B at green in B row, R column
+    B_at_G_in_R_B=G_at_R.T#B at green in R row, B column
+    B_at_R=R_at_B#B at red in R row, R column
+    h, w=raw_img.shape
+    colored_img=get_colored_img(raw_img).astype(np.float32)
+    red_mask, green_mask, blue_mask=np.dsplit(get_bayer_masks(h, w), 3)
+    green_mask=green_mask.squeeze()
+    red_mask=red_mask.squeeze()
+    green_mask=green_mask.squeeze()
+    alpha=1/2
+    beta=5/8
+    gamma=3/4
+    i, j=np.ogrid[:h, :w]
+    mask_at_R_B=(i%2+2*(j%2))==2
+    mask_at_B_R=(i%2+2*(j%2))==1
+    colored_img[1]+=alpha* apply_filter((colored_img*red_mask[..., np.newaxis])[...,0], G_at_R)*red_mask
+    colored_img[1]+=alpha* apply_filter((colored_img*blue_mask[..., np.newaxis])[...,2],G_at_B)*blue_mask
+    colored_img[0]+=beta*apply_filter((colored_img*mask_at_R_B[..., np.newaxis])[...,1], R_at_G_in_R_B)*mask_at_R_B
+    colored_img[0]+=beta* apply_filter((colored_img*mask_at_B_R[..., np.newaxis])[...,1], R_at_G_in_B_R)*mask_at_B_R
+    colored_img[0]+=beta* apply_filter((colored_img*blue_mask[..., np.newaxis])[...,2], R_at_B)*blue_mask
+    colored_img[2]+=gamma* apply_filter((colored_img*mask_at_B_R[..., np.newaxis])[...,1], B_at_G_in_B_R)*mask_at_B_R
+    colored_img[2]+=gamma* apply_filter((colored_img*mask_at_R_B[..., np.newaxis])[...,1], B_at_G_in_R_B)*mask_at_R_B
+    colored_img[2]+=beta* apply_filter((colored_img*red_mask[..., np.newaxis])[...,0], B_at_R)*red_mask
+    return colored_img
 
 def compute_psnr(img_pred, img_gt):
     """
@@ -98,8 +158,13 @@ def compute_psnr(img_pred, img_gt):
     :return:
         `float`, PSNR metric
     """
-    ...
-
+    img_pred=img_pred.astype("int32")
+    img_gt=img_gt.astype("int32")
+    h, w, c=img_pred.shape
+    mse=(1/(h*w*c)) * ((img_pred-img_gt)**2).sum()
+    if(mse==0):
+        raise ValueError("The images are identical. PSNR cannot be computed")
+    return 10 * log10((img_gt.max()**2)/mse)
 
 if __name__ == "__main__":
     from PIL import Image
--- Depth_Vision_and_Graphics/Prokudin-Gorsky/align.py
+++ Depth_Vision_and_Graphics/Prokudin-Gorsky/align.py
@@ -1,30 +1,78 @@
 import numpy as np
+from math import sqrt
 
 # Read the implementation of the align_image function in pipeline.py
 # to see, how these functions will be used for image alignment.
-
-
 def extract_channel_plates(raw_img, crop):
-    # Your code here
-    unaligned_rgb = (np.zeros(0), np.zeros(0), np.zeros(0))
-    coords = (np.array([0, 0]), np.array([0, 0]), np.array([0, 0]))
-    return unaligned_rgb, coords
+    h, w=raw_img.shape
+    small_h=h//3
+    up_coords = (np.array([0, 0]), np.array([small_h, 0]), np.array([2*small_h, 0]))
+    down_coords = (np.array([small_h, w]), np.array([2*small_h, w]), np.array([3*small_h, w]))
+    if crop:
+        up_coords=tuple(np.array([coord[0]+(small_h//10) , coord[1]+w//10]) for coord in up_coords)
+        down_coords=tuple(np.array([coord[0]-(small_h//10), coord[1]-w//10]) for coord in down_coords)
+    unaligned_rgb = (raw_img[up_coords[0][0]:down_coords[0][0], up_coords[0][1]:down_coords[0][1]], raw_img[up_coords[1][0]:down_coords[1][0], up_coords[1][1]:down_coords[1][1]], 
+    raw_img[up_coords[2][0]:down_coords[2][0], up_coords[2][1]:down_coords[2][1]])
+    ans_unaligned_rgb=(unaligned_rgb[2], unaligned_rgb[1], unaligned_rgb[0])#because in bgr order
+    ans_up_coords=(up_coords[2], up_coords[1], up_coords[0])
+    return ans_unaligned_rgb, ans_up_coords
 
+def Mse(img1,img2):
+    assert img1.shape==img2.shape
+    h, w=img1.shape
+    return  ((img1-img2)**2).sum()/(h*w)
+def Norm_cross_cor(img1, img2):
+    assert img1.shape==img2.shape
+    return (img1*img2).sum()/sqrt((img1**2).sum()*(img2**2).sum())
+def resize_by_2(img):
+    h, w=img.shape
+    h-=h%2
+    w-=w%2
+    return (img[:h:2, :w:2]+img[:h:2, 1:w:2]+img[1:h:2, :w:2]+img[1:h:2, 1:w:2])/4
+def calculate_metric_shift(img1, img2, shift, chosen_metric=Mse): #calculates metric between  img1 and shifted img2
+    assert img1.shape==img2.shape
+    x0, x1=max(0, shift[0]), min(img1.shape[0], img1.shape[0]+shift[0])
+    y0, y1=max(0, shift[1]), min(img1.shape[1], img1.shape[1]+shift[1])
+    shift=(-shift[0], -shift[1])
+    x2, x3=max(0, shift[0]), min(img1.shape[0], img1.shape[0]+shift[0])
+    y2, y3=max(0, shift[1]), min(img1.shape[1], img1.shape[1]+shift[1])
+    return chosen_metric(img1[x2:x3, y2:y3], img2[x0:x1, y0:y1])
+def find_relative_shift_pyramid(img_a, img_b, window_size=15,chosen_metric=Norm_cross_cor):
+    assert img_a.shape==img_b.shape
+    resized_images_a=[img_a]
+    resized_images_b=[img_b]
+    iterations=0
+    while(resized_images_a[0].shape[0]>500 or resized_images_a[0].shape[1]>500 or iterations==0):
+        resized_images_a.insert(0, resize_by_2(resized_images_a[0]))
+        resized_images_b.insert(0, resize_by_2(resized_images_b[0]))
+        iterations+=1
 
-def find_relative_shift_pyramid(img_a, img_b):
-    # Your code here
-    a_to_b = np.array([0, 0])
-    return a_to_b
-
+    shift=(0,0)
+    optimal_shift=shift
+    window_size=window_size//2+2
+    for _ in range(iterations+1):
+        img_a=resized_images_a.pop(0)
+        img_b=resized_images_b.pop(0)
+        optimal_shift=shift
+        optimal_metric=calculate_metric_shift(img_a, img_b, optimal_shift, chosen_metric)
+        for i in range(-window_size, window_size+1):
+            for j in range(-window_size, window_size+1):
+                check_shift=(shift[0]+i, shift[1]+j)
+                metric=calculate_metric_shift(img_a, img_b, check_shift, chosen_metric)
+                if((metric<optimal_metric and chosen_metric==Mse) or (metric>optimal_metric and chosen_metric==Norm_cross_cor)):
+                    optimal_metric=metric
+                    optimal_shift=check_shift
+        shift=(optimal_shift[0]*2, optimal_shift[1]*2)
+        window_size=1
+    return np.array(list(optimal_shift))    
 
 def find_absolute_shifts(
     crops,
     crop_coords,
     find_relative_shift_fn,
 ):
-    # Your code here
-    r_to_g = np.array([0, 0])
-    b_to_g = np.array([0, 0])
+    r_to_g = find_relative_shift_fn(crops[0], crops[1])+crop_coords[1]-crop_coords[0]
+    b_to_g = find_relative_shift_fn(crops[2], crops[1])+crop_coords[1]-crop_coords[2]
     return r_to_g, b_to_g
 
 
@@ -34,15 +82,41 @@ def create_aligned_image(
     r_to_g,
     b_to_g,
 ):
-    # Your code here
-    aligned_img = None
+    red_coords, green_coords, blue_coords=channel_coords
+    red_coords+=r_to_g
+    blue_coords+=b_to_g
+    red_coords-=green_coords
+    blue_coords-=green_coords
+    green_coords-=green_coords
+    img_shape=channels[0].shape
+    red_channel, green_channel, blue_channel=channels
+    x_green_0, y_green_0=np.dstack((red_coords, green_coords, blue_coords)).max(axis=-1).squeeze()
+    x_green_1, y_green_1=np.dstack((red_coords+img_shape, green_coords+img_shape, blue_coords+img_shape)).min(axis=-1).squeeze()
+    green_channel=green_channel[x_green_0:x_green_1, y_green_0:y_green_1]
+    green_coords-=red_coords
+    blue_coords-=red_coords
+    red_coords-=red_coords
+    x_red_0, y_red_0=np.dstack((red_coords, green_coords, blue_coords)).max(axis=-1).squeeze()
+    x_red_1, y_red_1=np.dstack((red_coords+img_shape, green_coords+img_shape, blue_coords+img_shape)).min(axis=-1).squeeze()
+    red_channel=red_channel[x_red_0:x_red_1, y_red_0:y_red_1]
+    green_coords-=blue_coords
+    red_coords-=blue_coords
+    blue_coords-=blue_coords
+    x_blue_0, y_blue_0=np.dstack((red_coords, green_coords, blue_coords)).max(axis=-1).squeeze()
+    x_blue_1, y_blue_1=np.dstack((red_coords+img_shape, green_coords+img_shape, blue_coords+img_shape)).min(axis=-1).squeeze()
+    blue_channel=blue_channel[x_blue_0:x_blue_1, y_blue_0:y_blue_1]
+    aligned_img = np.dstack((red_channel, green_channel, blue_channel))
     return aligned_img
 
 
 def find_relative_shift_fourier(img_a, img_b):
-    # Your code here
-    a_to_b = np.array([0, 0])
-    return a_to_b
+    C=np.fft.ifft2(np.fft.fft2(img_a)*np.conj(np.fft.fft2(img_b)))
+    flat_idx=np.argmax(np.real(C))
+    u, v=np.unravel_index(flat_idx, C.shape)
+    h, w=C.shape
+    u=(u+h//2)%h-h//2
+    v=(v+w//2)%w-w//2
+    return -np.array([u, v])
 
 
 if __name__ == "__main__":
